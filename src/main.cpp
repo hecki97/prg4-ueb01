c6b17e7a6d85af30c5084e9c7e96b3431e5b5160
Y.Heckmann <yaheckmann@htwsaar.de>
2020-05-13 22:02:48 +0200

    refactor: refactored main

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,11 @@
 #include <iomanip>
 #include <array>
 #include <fstream>
+#include <algorithm>
+#include <sstream>
 
 #include "utils.hpp"
+#include "io.hpp"
 
 #include "sorting_algorithms/insertionsort.hpp"
 #include "sorting_algorithms/mergesort.hpp"
@@ -25,7 +28,13 @@ mt19937 mt_rand;
 uniform_int_distribution<int> dist(0, 1'000'000);
 bool verbose = false;
 
-void run_sorting_suite(vector<int> vector, int* (*func_pointer)(int array[], size_t size), string name) {
+/**
+ Run the given sorting algorithm with the given vector and print results to std::cout
+ @param vector Unsorted vector
+ @param func_pointer Function pointer of sorting algorithm
+ @param name Name of sorting algorithm
+ */
+void run_sorting_algorithm(vector<int> vector, int* (*func_pointer)(int array[], size_t size), const string &name) {
     chrono::steady_clock::time_point start = chrono::steady_clock::now();
     func_pointer(&vector[0], vector.size());
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
@@ -38,36 +47,61 @@ void run_sorting_suite(vector<int> vector, int* (*func_pointer)(int array[], siz
     }
 }
 
+/**
+ Initalizes a vector of the given size with pseudorandom values and runs all defined sorting algorithms on it
+ @param size Size of vector
+ */
 void run_benchmark(size_t size) {
     // Fill array with pseudo-random values
     vector<int> vector;
     for (size_t i = 0; i < size; i++) {
         vector.push_back(dist(mt_rand));
     }
-    
-    utils::print_header("Running sorting suites on array of size", size);
-    
+
+    stringstream message;
+    message << "Running sorting suites on array of size " << size;
+    utils::print_header(message.str());
+
     if (verbose) {
         cout << "Unsorted array" << endl;
         utils::print_vector(vector);
     }
     
-    run_sorting_suite(vector, msort::sort, "MergeSort");
-    run_sorting_suite(vector, ssort::sort, "ShellSort");
-    run_sorting_suite(vector, bsort::sort, "BubbleSort");
-    run_sorting_suite(vector, isort::sort, "InsertionSort");
+    run_sorting_algorithm(vector, msort::sort, "MergeSort"s);
+    run_sorting_algorithm(vector, ssort::sort, "ShellSort"s);
+    run_sorting_algorithm(vector, bsort::sort, "BubbleSort"s);
+    run_sorting_algorithm(vector, isort::sort, "InsertionSort"s);
 }
 
-bool is_input_in_range(int input) {
-    return input > 0 && static_cast<size_t>(input) <= ARRAY_SIZES.size() + 3;
+/**
+ Tests if value is in array bounds
+ @param value Value to test
+ @returns true if value is in bounds, false otherwise
+ */
+bool is_input_in_range(int value) {
+    return value > 0 && static_cast<size_t>(value) <= ARRAY_SIZES.size() + 3;
 }
 
-bool is_input_non_negative(int input) {
-    return input > 0;
+/**
+ Prints the elapsed time to std::cout
+ @param name Name of sorting algorithm
+ @param elapsed_time Elapsed time in milliseconds
+*/
+void print_elapsed_time(const string &name, chrono::milliseconds &elapsed_time) {
+    cout
+        << setw(15) << left << name
+        << " took "
+        << setw(10) << right << elapsed_time.count()
+        << "ms."
+        << endl;
 }
 
+/**
+ Prints a selection menu to std::cout
+ @returns Selected menu item
+*/
 int selection_menu() {
-    utils::print_header("Sorting algorithm benchmark");
+    utils::print_header("Sorting algorithm benchmark"s);
     cout << "Run benchmark for:" << endl;
     
     size_t i {0};
@@ -81,7 +115,7 @@ int selection_menu() {
     cout << endl;
     
     int selection;
-    utils::safe_read(cin, cout, selection, is_input_in_range, "Selection: ");
+    io::safe_read(selection, is_input_in_range, "Selection: "s);
     return selection;
 }
 
@@ -94,13 +128,11 @@ int main() {
         if (selection <= ARRAY_SIZES.size()) {
             run_benchmark(ARRAY_SIZES[selection-1]);
         } else if (selection == ARRAY_SIZES.size() + 1) {
-            for (size_t i = 0; i < ARRAY_SIZES.size(); i++) {
-                run_benchmark(ARRAY_SIZES[i]);
-            }
+            for_each(ARRAY_SIZES.begin(), ARRAY_SIZES.end(), run_benchmark);
         } else if (selection == ARRAY_SIZES.size() + 2) {
             int size;
-            utils::safe_read(cin, cout, size, is_input_non_negative, "Number of elements: ");
-            verbose = utils::ask_for_verbose_logging();
+            io::safe_read(size, utils::is_input_greater_zero, "Number of elements: "s);
+            verbose = io::ask_for_verbose_logging();
             run_benchmark(size);
         } else {
             is_running = false;
