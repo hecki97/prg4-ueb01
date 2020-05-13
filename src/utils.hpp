7d3c01a0eb1609cb4e152753996c58bc091b985d
Y.Heckmann <yaheckmann@htwsaar.de>
2020-05-13 22:01:41 +0200

    refactor & doc: refactored utils and added documentation

--- src/utils.hpp
+++ src/utils.hpp
@@ -13,13 +13,52 @@
 #include <chrono>
 
 namespace utils {
-    void print_separator(int width = 50);
-    void print_header(std::string message);
-    void print_header(std::string message, size_t size);
-    void print_elapsed_time(std::string name, std::chrono::milliseconds elapsed_time);
-    void print_vector(std::vector<int> vector);
-    bool safe_read(std::istream& in, std::ostream& out, int& input, bool (*cond) (int), std::string message);
-    bool ask_for_verbose_logging();
+    /**
+     Tests if the input is greater zero
+     @param value Value that is tested
+     @returns true if value is greater than zero, false otherwise
+    */
+    bool is_input_greater_zero(int value);
+
+    /**
+     Prints a line consisting only of dashes ("-")
+     @param width number of dashes (default: 50 characters)
+     @param out where to write to  (default: std::cout)
+     */
+    void print_separator(int width = 50, std::ostream &out = std::cout);
+
+    /**
+     Prints the given message with a separator above and below
+     @param message The message to print
+     @param out  where to write to  (default: std::cout)
+     */
+    void print_header(const std::string &message, std::ostream &out = std::cout);
+
+    /**
+     Prints the elapsed time to the given output stream
+     @param name The name of the sorting algorithm
+     @param elapsed_time Elapsed time in milliseconds
+     */
+    void print_elapsed_time(const std::string &name, std::chrono::milliseconds &elapsed_time);
+
+    /**
+     Prints the content of the given vector and insert a linefeed after ten elements for improved readability
+     @param vector The vector to print
+     */
+    template <typename T>
+    void print_vector(std::vector<T> &vector) {
+        
+        size_t i {1};
+        for (T e : vector) {
+            std::cout << std::setw(6) << e << ", ";
+            
+            // Insert a linefeed every ten elements
+            if (i++ % 10 == 0) {
+                std::cout << std::endl;
+            }
+        }
+        std::cout << std::endl << std::endl;
+    }
 }
 
 #endif /* utils_hpp */
